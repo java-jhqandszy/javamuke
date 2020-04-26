48624c107da304e9edbe29df4cbbde8958ab2a9b
贾赫琦 <jiaheqi@hualala.com>
2020-04-26 17:29:10 +0800

    javamuke学习项目

---
+++ src/main/java/com/jhqszy/javamuke/JavamukeApplication.java
@@ -0,0 +1,13 @@
+package com.jhqszy.javamuke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamukeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavamukeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhqszy/javamuke/JavamukeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhqszy.javamuke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamukeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
